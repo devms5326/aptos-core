2899bae6b39c5089dcf0602bdb2861a27676d645
devms5326 <dev@movespiders.com>
2025-04-26 11:06:36 +0700

    upgrade gcp ver

--- terraform/aptos-node/gcp/versions.tf
+++ terraform/aptos-node/gcp/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0.0"
+      version = "~> 5.44.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.0.0"
+      version = "~> 5.44.1"
     }
     helm = {
       source = "hashicorp/helm"
